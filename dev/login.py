6162adb63948f79b04c04f29cc716c5292875e2b
kiran <kiranmsdb6@gmail.com>
2019-08-23 04:00:13 +0530

    my login first commit

---
+++ dev/login.py
@@ -0,0 +1,10 @@
+cd application
+$ alias git="docker run -ti --rm -v $(pwd):/git -v $HOME/.ssh:/root/.ssh alpine/git"
+$ git clone git@github.com:YOUR_ACCOUNT/YOUR_REPO.git
+$ cd YOUR_REPO
+$ alias git="docker run -ti --rm -v $(pwd):/git -v $HOME/.ssh:/root/.ssh alpine/git"
+# edit several files
+$ git add . 
+$ git status
+$ git commit -m "test"
+$ git push -u origin master
